447bed49f4636ef740f277e5383559548552a9a8
ldelossa <louis.delos@gmail.com>
2025-08-13 10:18:07 -0400

    wip

--- scripts/ztunnel-secrets.sh
+++ scripts/ztunnel-secrets.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-openssl genrsa -out private.key 2048
+# == Bootstrap ===
+openssl genrsa -out bootstrap-private.key 2048
 
-echo '[ req ]
+echo '
+[ req ]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_ca
 prompt = no
@@ -13,11 +15,40 @@ O = cluster.local
 [ v3_ca ]
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
-basicConstraints = critical,CA:TRUE
-keyUsage = critical,keyCertSign' > openssl.conf
+basicConstraints = CA:FALSE
+keyUsage = digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth, clientAuth
+subjectAltName = @alt_names
 
-openssl req -x509 -new -nodes -key private.key -sha256 -days 3650 -out root.crt -config openssl.conf
+[alt_names]
+DNS.1 = localhost
+' > openssl.conf
+
+openssl req -x509 -new -nodes -key bootstrap-private.key -sha256 -days 3650 -out bootstrap-root.crt -config openssl.conf
+
+# == CA ==
+openssl genrsa -out ca-private.key 2048
+
+echo '
+[ req ]
+distinguished_name = req_distinguished_name
+x509_extensions = v3_ca
+prompt = no
+
+[ req_distinguished_name ]
+O = cluster.local
+
+[ v3_ca ]
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always,issuer
+basicConstraints = critical, CA:true
+keyUsage = critical, digitalSignature, cRLSign, keyCertSign
+' > openssl.conf
+
+openssl req -x509 -new -nodes -key ca-private.key -sha256 -days 3650 -out ca-root.crt -config openssl.conf
 
 kubectl --namespace kube-system create secret generic cilium-ztunnel-secrets \
-      --from-file=private.key=private.key \
-      --from-file=root.crt=root.crt
+      --from-file=bootstrap-private.key=bootstrap-private.key \
+      --from-file=bootstrap-root.crt=bootstrap-root.crt \
+	  --from-file=ca-private.key=ca-private.key \
+	  --from-file=ca-root.crt=ca-root.crt
